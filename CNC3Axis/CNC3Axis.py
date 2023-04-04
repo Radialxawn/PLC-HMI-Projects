4e47dce376eeb9d5ff6d001addb9eccfe1457bb2
Nghiep <dinhnghiepzxc@gmail.com>
2023-04-04 16:25:54 +0700

    .

--- CNC3Axis/CNC3Axis.py
+++ CNC3Axis/CNC3Axis.py
@@ -33,7 +33,7 @@ class Axis:
       self._min = _min
       self._max = _max
       
-   def get_location(self, value):
+   def get_machine_location(self, value):
       return value - self._min
    
    def get_animation_location(self, value):
@@ -42,57 +42,104 @@ class Axis:
 axis_x = Axis()
 axis_y = Axis()
 axis_z = Axis()
+R, LS = 3, 0.01 # Bit radius, length step
+FSM, FS = 100, 20 # Feed speed max, feed speed
+
+def _to_machine_location(x, y, z):
+   return (axis_x.get_machine_location(x), axis_y.get_machine_location(y), axis_z.get_machine_location(z))
+
+def _get_range(a, b, step):
+   if a == b:
+      return [a]
+   step = step < 0 and -step or step
+   increase = a < b
+   result = []
+   i = a
+   count, count_max = 0, 1024
+   while True:
+      result.append(i)
+      i = increase and i + step or i - step
+      if increase:
+         if i >= b:
+            break
+      else:
+         if i <= b:
+            break
+      count += 1
+      if (count == count_max):
+         print('_get_range overflow')
+         return []
+   result.append(b)
+   return result
+
+def _run_code_C1(xStep, zStep):
+   interpolation.local_start()
+   #Top face
+   x = -9 - R
+   interpolation.line((x, 0, 0), FSM)
+   for iz in _get_range(1.25, 1.5, zStep):
+      z = iz
+      interpolation.line((x, 0, z), FSM)
+      for ix in _get_range(x, 1 - R + 1, xStep):
+         interpolation.arc((0, 0, z), (ix, 0, z), True, LS, FS)
+      z = 0
+      interpolation.line((x, 0, z), FSM)
+   #Mid
+   interpolation.line((x, 0, 0), FSM)
+   for iz in _get_range(1.6, 4.5, zStep):
+      z = iz
+      interpolation.line((x, 0, z), FSM)
+      k = -3.4 - R
+      for ix in _get_range(x, k, xStep):
+         interpolation.arc((0, 0, z), (ix, 0, z), True, LS, FS)
+      interpolation.arc((0, 0, z), (k, 0, z), True, LS, FS)
+      z = 0
+      interpolation.line((x, 0, z), FSM)
+   #Bottom
+   interpolation.line((x, 0, 0), FSM)
+   for iz in _get_range(4.6, 7, zStep):
+      z = iz
+      interpolation.line((x, 0, z), FSM)
+      kx = -5 - R
+      for ix in _get_range(x, kx, xStep):
+         interpolation.arc((0, 0, z), (ix, 0, z), True, LS, FS)
+      interpolation.arc((0, 0, z), (kx, 0, z), True, LS, FS)
+      z = 0
+      interpolation.line((x, 0, z), FSM)
+   #Curve
+   kx, ky, clockwise = -((16.5 + R) * math.sin(math.pi / 6)), -((16.5 + R) * math.cos(math.pi / 6) - 12), False
+   for iz in _get_range(4.6, 7, zStep):
+      z = iz
+      interpolation.line((clockwise and -kx or kx, ky, z), FSM)
+      interpolation.arc((0, 12, z), (clockwise and kx or -kx, ky, z), clockwise, LS, FS)
+      clockwise = not clockwise
+   interpolation.line((kx, ky, 0), FSM)
+   #
+   interpolation.local_end()
 
 def run_code():
-   R, LS = 3, 0.01 # Bit radius, length step
-   FSM, FS = 100, 20 # Feed speed max, feed speed
    interpolation.set_data(mathutils.Vector((axis_x.max, axis_y.max, axis_z.max)))
    interpolation.refresh()
    #C1
-   temp_vector = mathutils.Vector((axis_x.get_location(-41), axis_y.get_location(20), axis_z.get_location(0)))
-   interpolation.line(temp_vector, FSM)
-   interpolation.local_start()
-   X = -12
-   interpolation.line((X, 0, 0), FSM)
-   Z = 7
-   interpolation.line((X, 0, Z), FSM)
-   interpolation.arc((0, 0, Z), (-7 - R, 0, Z), True, LS, FS)
-   interpolation.arc((0, 0, Z), (-6 - R, 0, Z), True, LS, FS)
-   interpolation.arc((0, 0, Z), (-5 - R, 0, Z), True, LS, FS)
-   interpolation.arc((0, 0, Z), (-5 - R, 0, Z), True, LS, FS)
-   interpolation.line((-10.8336, -4.21366, Z), FSM)
-   interpolation.arc((0, 12, Z), (10.8336, -4.21366, Z), False, LS, FS)
-   interpolation.line((10.8336, -4.21366, 0), FSM)
-   interpolation.line((-10.8336, -4.21366, 0), FSM)
-   Z = 4.5
-   interpolation.line((-6 - R, 0, Z), FSM)
-   interpolation.arc((0, 0, Z), (-5 - R, 0, Z), True, LS, FS)
-   interpolation.arc((0, 0, Z), (-5 - R, 0, Z), True, LS, FS)
-   interpolation.arc((0, 0, Z), (-4 - R, 0, Z), True, LS, FS)
-   interpolation.arc((0, 0, Z), (-3.4 - R, 0, Z), True, LS, FS)
-   interpolation.arc((0, 0, Z), (-3.4 - R, 0, Z), True, LS, FS)
-   Z = 1.5
-   interpolation.line((-4 - R, 0, Z), FSM)
-   interpolation.arc((0, 0, Z), (-3 - R, 0, Z), True, LS, FS)
-   interpolation.arc((0, 0, Z), (-2 - R, 0, Z), True, LS, FS)
-   interpolation.arc((0, 0, Z), (-1 - R, 0, Z), True, LS, FS)
-   interpolation.arc((0, 0, Z), (0 - R, 0, Z), True, LS, FS)
-   interpolation.arc((0, 0, Z), (1 - R, 0, Z), True, LS, FS)
-   interpolation.line((0, 0, 0), FSM)
-   interpolation.local_end()
+   interpolation.line(_to_machine_location(-41, 20, 0), FSM)
+   _run_code_C1(1, 1)
+   #C1-End
+   '''
+   #D1
+   interpolation.line(_to_machine_position(-15, 21, 0), FSM)
    #AB2
-   temp_vector = mathutils.Vector((axis_x.get_location(-20.5), axis_y.get_location(-25), axis_z.get_location(0)))
-   interpolation.line(temp_vector, FSM)
+   interpolation.line(_to_machine_position(-20.5, -25, 0), FSM)
    #B1
-   temp_vector = mathutils.Vector((axis_x.get_location(20), axis_y.get_location(20), axis_z.get_location(0)))
-   interpolation.line(temp_vector, FSM)
+   interpolation.line(_to_machine_position(20, 20, 0), FSM)
    #AB1
-   temp_vector = mathutils.Vector((axis_x.get_location(50.5), axis_y.get_location(-25), axis_z.get_location(0)))
-   interpolation.line(temp_vector, FSM)
+   interpolation.line(_to_machine_position(50.5, -25, 0), FSM)
    #A1
-   temp_vector = mathutils.Vector((axis_x.get_location(63), axis_y.get_location(20), axis_z.get_location(0)))
-   interpolation.line(temp_vector, FSM)
+   interpolation.line(_to_machine_position(63, 20, 0), FSM)
    #End
+   '''
+   x, y, z = interpolation.get_location()
+   interpolation.line((x, y, 0), FSM)
+   interpolation.line((0, 0, 0), FSM)
    interpolation.check()
 
 def export_data():
@@ -106,8 +153,8 @@ def export_data():
    f.writelines(lines)
    f.close()
 
-def animate(target):
-   locations, frames = interpolation.animate(target, 24)
+def animate(target, timeFactor):
+   locations, frames = interpolation.animate(target, timeFactor)
    axis_x.object.animation_data_clear()
    axis_y.object.animation_data_clear()
    axis_z.object.animation_data_clear()
