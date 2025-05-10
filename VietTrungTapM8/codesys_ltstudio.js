5eb3db7a16d0fa2ed514ae8f00173bd33508ccde
Nghiep <dinhnghiepzxc@gmail.com>
2025-05-10 19:38:34 +0700

    .

--- VietTrungTapM8/codesys_ltstudio.js
+++ VietTrungTapM8/codesys_ltstudio.js
@@ -14,15 +14,10 @@ class AutoClass {
    }
 }
 
-const I_8bit = { name: 'I_8bit', region: 'I_8bit', type: 'Bit'};
-const QW = { name: 'QW', region: 'Q', type: 'Word', size: 1};
-const QD = { name: 'QD', region: 'Q', type: 'DWord', size: 2 };
-const MW = { name: 'MW', region: 'M', type: 'Word', size: 1 };
-const MD = { name: 'MD', region: 'M', type: 'DWord', size: 2 };
+const Q = { region: 'Q', single: { name: 'QW', name_bit: 'QX', type: 'Word', type_bit: 'Bit' }, double: { name: 'QD', type: 'DWord' } };
+const M = { region: 'M', single: { name: 'MW', name_bit: 'MX', type: 'Word', type_bit: 'Bit' }, double: { name: 'MD', type: 'DWord' } };
 const Auto = new AutoClass();
 
-const T_INTEGER = 'T_INTEGER';
-const T_UINTEGER = 'T_UINTEGER';
 const BOOL = 'BOOL';
 const INT = 'INT';
 const DINT = 'DINT';
@@ -30,95 +25,105 @@ const UINT = 'UINT';
 const UDINT = 'UDINT';
 
 let name__tag = {};
-let region_used = { Q: [], M: [], I_8bit: [] };
+let region_used = { Q: [], M: [] };
 let error = '';
 
-region_used.Q = Array(1024).fill(false);
-region_used.M = Array(1024).fill(false);
-region_used.I_8bit = [];
-for (var i = 0; i < 1024; i++) {
-   let region_byte = Array(8).fill(false);
-   region_used.I_8bit.push(region_byte);
+region_used.Q = [];
+region_used.M = [];
+for (var i = 0; i < 2048; i++) {
+   let q_byte = Array(8).fill(false);
+   region_used.Q.push(q_byte);
+   let m_byte = Array(8).fill(false);
+   region_used.M.push(m_byte);
 }
 
-function tag_add(_name_, _register_, _address_, _plc_type_) {
+function any_bit_used(_byte_) {
+   for (var i = 0; i < _byte_.length; i++) {
+      if (_byte_[i] == true) {
+         return true;
+      }
+   }
+   return false;
+}
+
+function byte_count_get(_type_) {
+   return [DINT, UDINT].includes(_type_) ? 4 : 2;
+}
+
+function tag_add_boolean_auto(_region_) {
+   for (var i = Auto.begin; i < _region_.length; i++) {
+      let byte = _region_[i];
+      for (var j = 0; j < byte.length; j++) {
+         if (byte[j] == false) {
+            return [i, j];
+         }
+      }
+   }
+   error += `Region limit reach\n`;
+}
+
+function tag_add_integer_auto(_region_, _type_) {
+   let size = byte_count_get(_type_);
+   let i_begin = Auto.begin * size;
+   let i_end = _region_.length - size;
+   for (var i = i_begin; i < i_end; i += size) {
+      let free = true;
+      for (var j = 0; j < size; j++) {
+         if (any_bit_used(_region_[i + j])) {
+            free = false;
+         }
+      }
+      if (free) {
+         return [i / size, 0];
+      }
+   }
+   error += `Region limit reach\n`;
+}
+
+function tag_add(_name_, _register_, _type_, _address_) {
    let region = region_used[_register_.region];
-   if (region == region_used.Q || region == region_used.M) {
+   // add
+   if (_type_ == BOOL) {
       if (_address_ == Auto) {
-         let found = false;
-         let i_begin = Auto.begin * _register_.size;
-         for (var i = i_begin; i < region.length - _register_.size; i += _register_.size) {
-            let free = true;
-            for (var s = 0; s < _register_.size; s++) {
-               if (region[i + s] == true) {
-                  free = false;
-               }
-            }
-            if (free) {
-               _address_ = [i / _register_.size, 0];
-               found = true;
-               break;
-            }
-         }
-         if (!found) {
-            error += `Region limit reach -> ${_name_}\n`;
-            return;
-         }
+         _address_ = tag_add_boolean_auto(region);
+      }
+   } else {
+      if (_address_ == Auto) {
+         _address_ = tag_add_integer_auto(region, _type_);
       } else if (typeof _address_ === 'number') {
          _address_ = [_address_, 0];
       } else {
          error += `Error define -> ${_name_}\n`;
          return;
       }
-   } else if (region == region_used.I_8bit) {
-      if (_address_ == Auto) {
-         let found = false;
-         for (var i = Auto.begin; i < region.length; i++) {
-            let region_byte = region[i];
-            for (var j = 0; j < region_byte.length; j++) {
-               if (region_byte[j] == false) {
-                  _address_ = [i, j];
-                  found = true;
-                  break;
-               }
-            }
-            if (found) {
-               break;
-            }
-         }
-         if (!found) {
-            error += `Region limit reach -> ${_name_}\n`;
-            return;
-         }
-      } else if (!(Array.isArray(_address_) && _address_.length == 2 && 0 <= _address_[1] && _address_[1] <= 7)) {
-         error += `Error define -> ${_name_}\n`;
-         return;
-      }
    }
+   // check
    if (_name_ in name__tag) {
       error += `Dupplicated -> ${_name_}\n`;
       return;
    } else {
-      let address = { main: _address_[0], sub: _address_[1] };
-      let tag = { register: _register_, address: address, plc_type: _plc_type_ };
-      if (region == region_used.Q || region == region_used.M) {
-         let i_begin = address.main * _register_.size;
-         let i_end = i_begin + _register_.size;
-         for (var i = i_begin; i < i_end; i++) {
-            if (region[i] == true) {
-               error += `Overlap region -> ${_name_}\n`;
-               return;
-            } else {
-               region[i] = true;
-            }
-         }
-      } else if (region == region_used.I_8bit) {
-         let region_byte = region[address.main];
-         if (region_byte[address.sub] == true) {
+      let address = { main: _address_[0], sub: _address_[1]};
+      let tag = { register: _register_, address: address, type: _type_ };
+      if (_type_ == BOOL) {
+         let byte = region[address.main];
+         if (byte[address.sub] == true) {
             error += `Overlap region -> ${_name_}\n`;
             return;
          } else {
-            region_byte[address.sub] = true;
+            byte[address.sub] = true;
+         }
+      } else {
+         let size = byte_count_get(_type_);
+         let i_begin = address.main * size;
+         let i_end = i_begin + size;
+         for (var i = i_begin; i < i_end; i++) {
+            let byte = region[i];
+            if (any_bit_used(byte)) {
+               error += `Overlap region -> ${_name_}\n`;
+               return;
+            } else {
+               byte.fill(true);
+            }
          }
       }
       name__tag[_name_] = tag;
@@ -127,35 +132,28 @@ function tag_add(_name_, _register_, _address_, _plc_type_) {
 
 function hmi_csv_build() {
    let result = 'Sn,LabelName,DeviceAlias,RegName,RegisterTypes,StationNo,IsUseStationNoIndex,MainAddress,SubAddress,BlockAddress,IsUseBlockAddressIndex,UseWordAsBit,StationAddressDeviceAlias,StationAddressRegName,StationAddressRegisterTypes,StationAddressStationNo,StationAddressMainAddress,StationAddressBlockAddress,BlockAddressDeviceAlias,BlockAddressRegName,BlockAddressRegisterTypes,BlockAddressStationNo,BlockAddressMainAddress,BlockAddressBlockAddress,PlcId,StationAddressPlcId,BlockAddressPlcId,Version';
-   result += '\n';
    for (let [k, v] of Object.entries(name__tag)) {
-      result += `0,${k},PLC:[Ethernet PLC:Leadshine MC_Ethernet],${v.register.name},${v.register.type},1,False,${v.address.main},${v.address.sub},0,False,False,,,,,,,,,,,,,8205,,,1`;
+      let reg = ([BOOL, INT, UINT].includes(v.type)) ? v.register.single : v.register.double;
+      let n = v.type == BOOL ? reg.name_bit : reg.name;
+      let t = v.type == BOOL ? reg.type_bit : reg.type;
       result += '\n';
+      result += `0,${k},PLC:[Ethernet PLC:Leadshine MC_Ethernet],${n},${t},1,False,${v.address.main},${v.address.sub},0,False,False,,,,,,,,,,,,,8205,,,1`;
    }
    return result;
 }
 
 function plc_txt_build() {
    let result = '';
-   let hmi_map_plc = { QW: 'QW', QD: 'QD', MW: 'MW', MD: 'MD', I_8bit: 'QX' };
    for (let [k, v] of Object.entries(name__tag)) {
-      let region = region_used[v.register.region];
-      let r = '';
-      let n = hmi_map_plc[v.register.name];
-      if (region == region_used.Q || region == region_used.M) {
-         r = `${n}${v.address.main}`;
-         let single = v.register.size == 1;
-         if (v.plc_type == T_INTEGER) {
-            v.plc_type = single ? INT : DINT;
-         } else if (v.plc_type == T_UINTEGER) {
-            v.plc_type = single ? UINT : UDINT;
-         }
-      } else if (region == region_used.I_8bit) {
-         r = `${n}${v.address.main}.${v.address.sub}`;
-         v.plc_type = BOOL;
+      let reg = ([BOOL, INT, UINT].includes(v.type)) ? v.register.single : v.register.double;
+      let r = ``;
+      if (v.type == BOOL) {
+         r += `${reg.name_bit}${v.address.main}.${v.address.sub}`;
+      } else {
+         r += `${reg.name}${v.address.main}`;
       }
-      result += `\t_${k} AT %${r}: ${v.plc_type};`;
-      result += '\n';
+      result += result.length > 0 ? '\n' : '';
+      result += `\t_${k} AT %${r}: ${v.type};`;
    }
    return result;
 }
@@ -176,17 +174,51 @@ function save(_path_, _data_) {
 }
 
 /////GENERATE
-tag_add('Start', I_8bit, [0, 0]);
-tag_add('Stop', I_8bit, [0, 1]);
-tag_add('Damn', I_8bit, Auto);
-tag_add('Suck', I_8bit, Auto);
-tag_add('Holy', I_8bit, Auto.begin_set(10));
-tag_add('Yo', I_8bit, Auto.begin_set(20));
-tag_add('Output', QW, 0, T_UINTEGER);
-tag_add('A', QD, Auto.begin_set(20), T_INTEGER);
-tag_add('B', QD, Auto, T_INTEGER);
-tag_add('C', MD, Auto, T_INTEGER);
-tag_add('D', MD, Auto, T_INTEGER);
+tag_add('PLCReady', M, BOOL, Auto);
+tag_add('HMIReady', M, BOOL, Auto);
+tag_add('On', M, BOOL, Auto);
+tag_add('Off', M, BOOL, Auto);
+tag_add('Home', M, BOOL, Auto);
+tag_add('SettingApply', M, BOOL, Auto);
+tag_add('OverloadResolved', M, BOOL, Auto);
+//setting
+tag_add(`HomeTorque`, M, UDINT, Auto);
+tag_add(`AccelRpm`, M, UINT, Auto);
+tag_add(`DecelRpm`, M, UINT, Auto);
+for (var k of ['Tap']) {
+   tag_add(`${k}Begin`, M, UDINT, Auto);
+   tag_add(`${k}Depth`, M, UDINT, Auto);
+   tag_add(`${k}PeckDepth`, M, UDINT, Auto);
+   tag_add(`${k}Pitch`, M, UINT, Auto);
+   tag_add(`${k}Rpm`, M, UINT, Auto);
+   tag_add(`${k}TorqueMax`, M, UDINT, Auto);
+}
+for (var k of ['FoilExtract']) {
+   tag_add(`${k}`, M, BOOL, Auto);
+   for (var a of ['Top', 'Bot']) {
+      for (var b of ['N', 'P']) {
+         tag_add(`${k}${a}${b}Delay`, M, UINT, Auto);
+      }
+   }
+}
+for (var k of ['AirBurst']) {
+   tag_add(`${k}`, M, BOOL, Auto);
+   tag_add(`${k}Delay`, M, UINT, Auto);
+}
+//view
+for (var k of ['View']) {
+   for (var a of ['Linear', 'Rotary']) {
+      tag_add(`${k}${a}Position`, M, DINT, Auto);
+      tag_add(`${k}${a}Torque`, M, DINT, Auto);
+   }
+   tag_add(`${k}TapEnd`, M, UDINT, Auto);
+   tag_add(`${k}TapPeckEnd`, M, UDINT, Auto);
+   tag_add(`${k}FoilExtract`, M, BOOL, Auto);
+   tag_add(`${k}AirBurst`, M, BOOL, Auto);
+   tag_add(`${k}CycleTime`, M, UDINT, Auto);
+   tag_add(`${k}Overload`, M, BOOL, Auto);
+   tag_add(`${k}WorkReady`, M, BOOL, Auto);
+}
 /////GENERATE
 
 if (error == '') {
