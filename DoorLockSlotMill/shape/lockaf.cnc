cad8be82a93c0f0d2b919b685f5dc601b427e3ca
Radialxawn <46078012+Radialxawn@users.noreply.github.com>
2026-06-01 17:44:04 +0700

    .

---
+++ DoorLockSlotMill/shape/lockaf.cnc
@@ -0,0 +1,14 @@
+LET #a : LREAL := MAX{0.001, {$VA$-$VT$}*0.5}
+LET #r : LREAL := MAX{0.001, {$VB$-$VT$}*0.5}
+LET #rc : LREAL := MAX{0.001, {$VC$-$VT$}*0.5}
+N3 G0 X-#a
+N4 G1 Z$VZ$
+N5 G2 X-#a+#r Y#r R#r
+N6 G1 X#a-#r
+N7 G2 Y-#r R#r
+N8 G1 X-#a+#r
+N9 G2 X-#a Y0 R#r
+N10 G2 X-#a+#rc*2 Y0 R#rc
+N11 G2 X-#a R#rc
+N12 G0 Z0
+N13 G0 X0 Y0
--- DoorLockSlotMill/shape/none.py
+++ DoorLockSlotMill/shape/none.py
@@ -86,6 +86,25 @@ shape = {
 		'LET #r : LREAL := MAX{0.001, {$VB$-$VT$}*0.5}',
 		'LET #rc : LREAL := MAX{0.001, {$VC$-$VT$}*0.5}',
 		# capsule
+		'G0 X#a',
+		'G1 Z$VZ$',
+		'G2 X#a-#r Y-#r R#r',
+		'G1 X-#a+#r',
+		'G2 Y#r R#r',
+		'G1 X#a-#r',
+		'G2 X#a Y0 R#r',
+		# circle
+		'G2 X#a-#rc*2 Y0 R#rc',
+		'G2 X#a R#rc',
+		#
+		'G0 Z0',
+		'G0 X0 Y0',
+	],
+	'lockaf' : [
+		'LET #a : LREAL := MAX{0.001, {$VA$-$VT$}*0.5}',
+		'LET #r : LREAL := MAX{0.001, {$VB$-$VT$}*0.5}',
+		'LET #rc : LREAL := MAX{0.001, {$VC$-$VT$}*0.5}',
+		# capsule
 		'G0 X-#a',
 		'G1 Z$VZ$',
 		'G2 X-#a+#r Y#r R#r',
@@ -94,9 +113,8 @@ shape = {
 		'G1 X-#a+#r',
 		'G2 X-#a Y0 R#r',
 		# circle
-		'G1 X#a-#rc*2 Y0',
-		'G2 X#a R#rc',
-		'G2 X#a-#rc*2 R#rc',
+		'G2 X-#a+#rc*2 Y0 R#rc',
+		'G2 X-#a R#rc',
 		#
 		'G0 Z0',
 		'G0 X0 Y0',
